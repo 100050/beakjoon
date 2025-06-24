abfd7bff9dbdf56bd766b582e9658717cd9f76bb
100050 <82698917+100050@users.noreply.github.com>
2025-06-24 21:25:43 +0900

    [Bronze IV] Title: 수학은 체육과목 입니다 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <cstdlib>   
+#include <ctime> 
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<double, double>;
+
+
+void solve() {
+    int n;
+    int arr[8] = { 2, 1, 2, 3, 4 ,5, 4, 3 };
+    cin >> n;
+    
+
+
+    cout << arr[n % 8];
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
