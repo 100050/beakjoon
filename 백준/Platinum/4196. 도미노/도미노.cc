75bef25a98b9dee60bf6032631e5befb10e140d8
100050 <82698917+100050@users.noreply.github.com>
2025-08-07 23:33:03 +0900

    [Platinum IV] Title: 도미노, Time: 172 ms, Memory: 35568 KB -BaekjoonHub

---
+++ 백준/Platinum/4196. 도미노/도미노.cc
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+const ll MOD = 1000000007;
+const ld PI = acos(-1.0L);
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, -1, 0, 1 };
+
+void dfs(int node, vector<vector<int>>& g, vector<bool>& visited) {
+    visited[node] = true;
+
+    for (int next : g[node]) {
+        if (visited[next]) continue;
+        
+        dfs(next, g, visited);
+    }
+}
+
+int scc(int node, vector<vector<int>>& g, vector<int>& par, int& id, stack<int>& s, vector<vector<int>>& SCC, vector<bool>& finished, vector<bool>& is) {
+    par[node] = ++id;
+
+    
+    s.push(node);
+    int parent = par[node];
+
+    for (int next : g[node]) {
+        if (par[next] == 0)
+            parent = min(parent, scc(next, g, par, id, s, SCC, finished, is));
+        else if (!finished[next])
+            parent = min(parent, par[next]);
+    }
+
+    if (par[node] == parent) {
+        is[parent] = true;
+        vector<int> sc;
+        while (node) {
+            int t = s.top();  s.pop();
+
+            finished[t] = true;
+            sc.push_back(t);
+            par[t] = parent;
+            if (t == node) break;
+        }
+        SCC.push_back(sc);
+    }
+
+    return parent;
+}
+
+void solve(int CASE = -1) {
+    int n, m, res;
+    vector<vector<int>> g;
+    vector<int> ind;
+    vector<int> par;
+    int id;
+
+    res = 0;
+    id = 0;
+    cin >> n >> m;
+    g.resize(n + 1);
+    ind.resize(n + 1);
+    par.resize(n + 1);
+    while (m--) {
+        int x, y;
+        cin >> x >> y;
+        g[x].push_back(y);
+        ind[y]++;
+    }
+
+    // SCC
+    stack<int> s;
+    vector<vector<int>> SCC;
+    vector<bool> finished(n + 1), is(n + 1);
+    for (int i = 1; i <= n; i++) {
+        if (par[i] == 0) scc(i, g, par, id, s, SCC, finished, is);
+    }
+
+    vector<vector<int>> g2;
+    vector<int> ind2(n + 1);
+    g2.resize(n + 1);
+    for (vector<int>& t : SCC) {
+        for (int i : t) {
+            for (int next : g[i]) {
+                if (par[i] == par[next]) continue;
+
+                g2[par[i]].push_back(par[next]);
+                ind2[par[next]]++;
+                //cout << i << " " << next << " " << ind2[par[next]] << "\n";
+            }
+        }
+    }
+
+    // 위상 정렬
+    vector<int> arr;
+    queue<int> q;
+    for (int i = 1; i <= n; i++) {
+        if (ind2[i] == 0 && is[i]) {
+            q.push(i);
+            arr.push_back(i);
+        }
+    }
+    
+    while (q.size()) {
+        int f = q.front(); q.pop();
+        //cout << f << "\n";
+        for (int next : g2[f]) {
+            if (--ind2[next] == 0) q.push(next), arr.push_back(next);
+        }
+    }
+
+    vector<bool> visited(n + 1);
+
+    // 모든 정점 순회
+    for (int i = 0; i < arr.size(); i++)
+        if (!visited[arr[i]]) res++, dfs(arr[i], g2, visited);
+
+    cout << res << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
