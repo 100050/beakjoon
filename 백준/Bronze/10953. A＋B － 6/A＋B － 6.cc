37c8b80b4e4b8fad56f4dca781906f1162dfaa31
100050 <82698917+100050@users.noreply.github.com>
2025-09-13 13:19:27 +0900

    [Bronze III] Title: A+B - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t, a, b;
+    char x;
+    cin >> t;
+    while (t--) {
+    cin >> a >> x >> b;
+    cout << a + b << "\n";
+    }
+    return 0;
+}
