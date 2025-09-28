56e50c75ec7f6cd46811894746fd694b18693bb5
100050 <82698917+100050@users.noreply.github.com>
2025-09-28 13:55:13 +0900

    [Silver IV] Title: 행렬 게임, Time: 188 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Silver/32822. 행렬 게임/행렬 게임.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+const ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, -1, 0, 1 };
+
+ll diff = 0;
+int n, m;
+int a[1001][1001];
+int mx[1001];
+
+void solve(int CASE = -1) {
+
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            int t;
+            cin >> t;
+            a[i][j] = abs(a[i][j] - t);
+            mx[j] = max(mx[j], a[i][j]);
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        int beta;
+        cin >> beta;
+
+
+        diff += mx[beta];
+    }
+
+    cout << diff;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
