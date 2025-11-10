c4a7b674fa679a0ad7973774066ea47ee60620da
100050 <82698917+100050@users.noreply.github.com>
2025-11-10 15:22:25 +0900

    [Gold III] Title: $p^{n}!$과 쿼리, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/33680. $p＾｛n｝！$과 쿼리/$p＾｛n｝！$과 쿼리.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max();
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, -1, 0, 1 };
+
+ll power(ll a, ll b, ll mod = MOD) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (a * res) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+
+    return res % mod;
+}
+
+ll inv(ll a, ll mod = MOD) {
+    return power(a, mod - 2, mod) % mod;
+}
+
+void solve(int CASE = -1) {
+    ll p, n;
+
+    cin >> p >> n;
+
+    if (n == 1) cout << 1 << "\n";
+    else cout << (((power(p, n) - 1 + MOD) % MOD) * (inv(p - 1) % MOD)) % MOD << "\n";
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
