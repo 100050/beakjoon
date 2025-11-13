086c6fc80d9d7382a3f83fde96b1f2f50005c530
100050 <82698917+100050@users.noreply.github.com>
2025-11-13 12:32:39 +0900

    [Silver II] Title: 제곱수의 합, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max();
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, -1, 0, 1 };
+
+int n;
+int dp[100001];
+
+void solve(int CASE = -1) {
+    cin >> n;
+    fill(dp, dp + n + 1, INF<int>);
+    dp[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j*j <= i; j++) {
+            //cout << dp[i] << " " << dp[i-j]+1 << "\n";
+            dp[i] = min(dp[i], dp[i - j*j] + 1);
+        }
+    }
+
+    cout << dp[n];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
