bf51bb1971a05e0d262f6607f4442be2efb23190
100050 <82698917+100050@users.noreply.github.com>
2025-11-15 00:23:47 +0900

    [Silver V] Title: Joint Attack, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17910. Joint Attack/Joint Attack.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max();
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, -1, 0, 1 };
+
+void solve(int CASE = -1) {
+    int n;
+    stack<int> x;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+        x.push(t);
+    }
+
+    ll a = x.top(), b = 1; // c+b/a
+    x.pop();
+    while (x.size()) {
+        ll c = x.top(); x.pop();
+        c = c * a + b;
+        ll g = gcd(a, c);
+        b = a / g;
+        a = c / g;
+    }
+
+    cout << a << "/" << b;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
