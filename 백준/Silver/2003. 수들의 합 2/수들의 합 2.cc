c1750f9587715152f3ef534876da3301c122bd8c
김인환 <82698917+100050@users.noreply.github.com>
2025-12-14 19:31:50 +0900

    [Silver IV] Title: 수들의 합 2, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max();
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, -1, 0, 1 };
+
+void solve(int CASE = -1) {
+    int n, m;
+    int s = 0, c = 0;
+    vector<int> arr;
+
+    cin >> n >> m;
+    arr.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int left = 0, right = 0;
+    while (right < n) {
+        s += arr[right++];
+
+        while (s > m) {
+            s -= arr[left++];
+        }
+        
+        if (s == m) {
+            c++;
+        }
+    }
+
+    cout << c;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
