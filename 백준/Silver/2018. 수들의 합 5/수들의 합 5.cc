d1775e30c8c3ffeb803dfe7d36344efa0860425b
김인환 <82698917+100050@users.noreply.github.com>
2025-12-15 12:56:18 +0900

    [Silver V] Title: 수들의 합 5, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max();
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, -1, 0, 1 };
+
+void solve(int CASE = -1) {
+    int n;
+
+    cin >> n;
+
+    int sum = 0, j = 1, c = 1; // n으로만 구성된 자연수 합
+    for (int i = 1; i <= (n + 1) / 2 && n != 1; i++) { // n이 포함되지 않은 자연수들의 합
+        sum += i;
+
+        while (n < sum) sum -= j++;
+        if (n == sum) c++;
+    }
+
+    cout << c;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
