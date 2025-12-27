3ea45110c2373eb677e00d975d3133bf1179a860
김인환 <82698917+100050@users.noreply.github.com>
2025-12-27 21:15:21 +0900

    [Gold III] Title: PIZZA ALVOLOC, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12781. PIZZA ALVOLOC/PIZZA ALVOLOC.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max();
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, -1, 0, 1 };
+
+int ccw(const pii& a, const pii& b, const pii& c) { 
+    int op = a.first * b.second + b.first * c.second + c.first * a.second;
+    op -= (a.second * b.first + b.second * c.first + c.second * a.first);
+
+    if (op > 0) return 1;
+    else if (op == 0)return 0;
+    else return -1; 
+}
+
+void solve(int CASE = -1) {
+    pii arr[4];
+
+    for (int i = 0;  i < 4; i++)
+        cin >> arr[i].first >> arr[i].second;
+
+    int res = ccw(arr[0], arr[1], arr[2]) * ccw(arr[0], arr[1], arr[3]);
+
+    if (res < 0) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
