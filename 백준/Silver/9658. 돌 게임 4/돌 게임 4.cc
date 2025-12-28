e548f0c89d3aa13851d6607720be223eabd4f549
김인환 <82698917+100050@users.noreply.github.com>
2025-12-28 22:04:13 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max();
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, -1, 0, 1 };
+
+int dp[1001][2];
+
+int dfs(int n, bool t) {
+    if (n == 1 || n == 3) return 0;
+    if (n == 2 || n == 4) return 1;
+
+    int& ret = dp[n][t];
+    if (ret != -1) return ret;
+
+    return ret = (dfs(n - 1, !t) == 0) || (dfs(n - 3, !t) == 0) || (dfs(n - 4, !t) == 0);
+}
+
+void solve(int CASE = -1) {
+    int n;
+
+    cin >> n;
+
+    fill(&dp[0][0], &dp[1000][2], -1);
+
+    if (dfs(n, 1)) {
+        cout << "SK";
+    }
+    else {
+        cout << "CY";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
