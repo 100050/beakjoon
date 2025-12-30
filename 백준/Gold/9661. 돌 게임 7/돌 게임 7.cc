83d52c5843b271e59af6011496280b4a04625562
김인환 <82698917+100050@users.noreply.github.com>
2025-12-30 16:29:38 +0900

    [Gold II] Title: 돌 게임 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9661. 돌 게임 7/돌 게임 7.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max();
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, -1, 0, 1 };
+
+void solve(int CASE = -1) {
+    ll n;
+
+    cin >> n;
+
+    int arr[5]{ 1, 0, 1, 1, 0 };
+    if (arr[(n - 1) % 5]) {
+        cout << "SK";
+    }
+    else {
+        cout << "CY";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
