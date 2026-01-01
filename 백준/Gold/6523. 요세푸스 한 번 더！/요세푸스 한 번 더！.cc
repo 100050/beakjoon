5eb9a4c4e8d83984918be643346dfc0777c5cfb8
김인환 <82698917+100050@users.noreply.github.com>
2026-01-01 14:47:16 +0900

    [Gold II] Title: 요세푸스 한 번 더!, Time: 1068 ms, Memory: 12228 KB -BaekjoonHub

---
+++ 백준/Gold/6523. 요세푸스 한 번 더！/요세푸스 한 번 더！.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max();
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, -1, 0, 1 };
+
+void solve(int CASE = -1) {
+    ll n, a, b;
+
+    while (true) {
+        cin >> n;
+
+        if (n == 0) break;
+
+        cin >> a >> b;
+        ll x = 0;
+        unordered_set<ll> s, ss;
+       
+        while (ss.find(x) == ss.end()) {
+            if (s.find(x) == s.end()) {
+                s.insert(x);
+            }
+            else {
+                ss.insert(x);
+            }
+            x = (((a * ((x * x) % n)) % n) + b) % n;
+        }
+        
+
+        cout << n - ss.size() << "\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
