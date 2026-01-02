300f824499d2218a9933e4ceb9aa281aae29c6e2
김인환 <82698917+100050@users.noreply.github.com>
2026-01-02 21:11:38 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 92 ms, Memory: 4368 KB, Score: 250 point -BaekjoonHub

---
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max();
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, -1, 0, 1 };
+
+ll power(ll a, ll b, ll mod = MOD) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (a * res) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+
+    return res % mod;
+}
+void solve(int CASE = -1) {
+    int n;
+    vector<ll> arr;
+
+    cin >> n;
+    arr.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+
+    ll res = 0;
+    for (int i = 0; i < n; i++) {
+        res = (res + arr[i] * (power(2, i) - power(2, n - i - 1))) % MOD;
+    }
+    
+    cout << res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
