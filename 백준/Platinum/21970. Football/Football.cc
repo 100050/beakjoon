1be3c4486ab4dc0616d91ec249f709106576d40c
김인환 <82698917+100050@users.noreply.github.com>
2026-01-13 16:05:25 +0900

    [Platinum III] Title: Football, Time: 36 ms, Memory: 2220 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Platinum/21970. Football/Football.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max();
+const int dx[4] = { 0, -1, 1, 0 };
+const int dy[4] = { -1, 0, 0, 1 };
+
+void solve(int CASE = -1) {
+    int n, k;
+    vector<int> arr;
+
+    cin >> n >> k;
+    arr.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    while (k) {
+        ll s = 0;
+        for (int j = 0; j < n; j++) {
+            s += arr[j];
+            arr[j] /= 2;
+        }
+
+        if (s % 2LL == 1LL) {
+            cout << 1;
+
+            return;
+        }
+        k /= 2;
+    }
+
+    cout << 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
