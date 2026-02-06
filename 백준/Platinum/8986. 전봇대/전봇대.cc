2960df54ec0a7df02049a525b944988529fba38d
김인환 <82698917+100050@users.noreply.github.com>
2026-02-06 19:44:38 +0900

    [Platinum V] Title: 전봇대, Time: 20 ms, Memory: 2804 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Platinum/8986. 전봇대/전봇대.cc
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max(); // 오버플로우 조심
+const int dx[4] = { 0, -1, 1, 0 };
+const int dy[4] = { -1, 0, 0, 1 };
+
+int n;
+vector<ll> arr;
+
+ll f(ll d) {
+    ll ret = 0;
+    for (ll i = 0; i < n; i++) {
+        ret += abs(i * d - arr[i]);
+    }
+
+    return ret;
+}
+
+void solve(int CASE = -1) {
+
+    cin >> n;
+    arr.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    ll hi = arr[n - 1], lo = 0;
+    while (hi - lo >= 3) {
+        ll p = (hi + lo * 2) / 3, q = (hi * 2 + lo) / 3;
+
+        if (f(p) > f(q)) {
+            lo = p;
+        }
+        else {
+            hi = q;
+        }
+    }
+
+    ll res = INF<ll>;
+    for (ll i = lo; i <= hi; i++) {
+        res = min(res, f(i));
+    }
+
+    cout << res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+
+    }
+
+    return 0;
+}
