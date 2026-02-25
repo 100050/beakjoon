1c12beb8b31d6c5b35319c179f9edd6f33eadd68
김인환 <82698917+100050@users.noreply.github.com>
2026-02-26 01:41:13 +0900

    [Gold III] Title: Double It, Time: 300 ms, Memory: 5224 KB -BaekjoonHub

---
+++ 백준/Gold/28703. Double It/Double It.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//const ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max(); // 오버플로우 조심
+const int dx[4] = { 0, -1, 1, 0 };
+const int dy[4] = { -1, 0, 0, 1 };
+
+void solve(int CASE = -1) {
+    int n;
+    ll mx = 0;
+    priority_queue<ll, vector<ll>, greater<ll>> pq;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        ll t;
+        cin >> t;
+        pq.push(t);
+        mx = max(t, mx);
+    }
+
+    ll mn = INF<ll>, c = 0;
+    while (c != n) {
+        ll t = pq.top(); pq.pop();
+
+        if (mn > abs(mx - t)) {
+            mn = abs(mx - t);
+            c = 0;
+        }
+        else {
+            c++;
+        }
+        mx = max(mx, 2 * t);
+        pq.push(2 * t);
+    }
+    
+    cout << mn;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+
+    }
+
+    return 0;
+}
