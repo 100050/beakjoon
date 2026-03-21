1253985d64dbec2ffba0a2bc601373468ea48d02
김인환 <82698917+100050@users.noreply.github.com>
2026-03-21 21:42:25 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    ll n;
+    ll dp[91] = { 0, 1, };
+    
+    cin >> n;
+    for (int i = 2; i <= n; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
+
+    cout << dp[n];
+
+    return 0;
+}
