06d5dbd2a9cf82bb229781cdb1cc9e9d901bb1b3
김인환 <82698917+100050@users.noreply.github.com>
2026-04-07 20:29:45 +0900

    [Platinum III] Title: Xor Maximization, Time: 40 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Platinum/11191. Xor Maximization/Xor Maximization.cc
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <ctime>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+#include <bit>
+using namespace std;
+
+using ull = unsigned long long;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
+
+constexpr ll MOD = 1000000007;
+//constexpr ll MOD = 10007;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max(); // 오버플로우 조심
+const int dx[4] = { 0, -1, 1, 0 };
+const int dy[4] = { -1, 0, 0, 1 };
+
+void solve(int CASE = -1) {
+    int n;
+    vector<ull> arr;
+
+    cin >> n;
+    arr.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int rank = 0;
+    for (int j = 59; j >= 0 && rank < n; j--) {
+        if (~arr[rank] & (1uLL << j)) {
+            int idx = -1;
+            for (int i = rank + 1; i < n; i++) {
+                if (arr[i] & (1uLL << j)) { idx = i; break; }
+            }
+
+            if (idx == -1) continue; // 문제에 따라 처리 다름
+            arr[rank] ^= arr[idx];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i == rank) continue;
+            if (arr[i] & (1LL << j)) arr[i] ^= arr[rank];
+        }
+        rank++;
+    }
+
+    ull res = 0;
+    for (int i = 0; i < n; i++) res ^= arr[i];
+
+    cout << res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    //pre();
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
