4d7944793fe500b43f4af7e2db1cca6e2c5c533f
김인환 <82698917+100050@users.noreply.github.com>
2026-04-19 22:27:20 +0900

    [Diamond IV] Title: 트리와 쿼리 8, Time: 404 ms, Memory: 159100 KB -BaekjoonHub

--- 백준/Diamond/13517. 트리와 쿼리 8/트리와 쿼리 8.cc
+++ 백준/Diamond/13517. 트리와 쿼리 8/트리와 쿼리 8.cc
@@ -1,158 +1,210 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include <algorithm>
+#include <string>
 #include <cmath>
-
+#include <map>
+#include <set>
+#include <ctime>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <ranges>
+#include <numeric>
+#include <cstring>
+#include <bit>
 using namespace std;
 
+using ull = unsigned long long;
 using ll = long long;
+using ld = long double;
 using pii = pair<int, int>;
+using pil = pair<ll, ll>;
+using pid = pair<ld, ld>;
 
-const int MAXN = 100005;
-const int INF = 1e9 + 7;
+constexpr ll MOD = 1000000007;
+//constexpr ll MOD = 1000000000;
+const ld PI = acos(-1.0L);
+template<typename T>
+constexpr T INF = numeric_limits<T>::max(); // 오버플로우 조심
+const int dx[4] = { 0, -1, 1, 0 };
+const int dy[4] = { -1, 0, 0, 1 };
+
+
+using T = int;
 
 struct Node {
-    int l, r, v;
+	int l, r;
+	T v;
 };
 
-int n, m, cnt = 0;
-vector<int> adj[MAXN], tour, n2s(1), firstTour(MAXN), root, d, par(MAXN), sorted_d;
-vector<pii> range_val(MAXN);
-vector<Node> st;
-vector<int> st2;
+int cnt = 0;
+vector<pii> range;
+vector<int> adj[100'001], tour, firstTour, n2s(1);
 
-// PST 초기화
-int make_st(int start, int end) {
-    int node = st.size();
-    st.push_back({-1, -1, 0});
-    if (start == end) return node;
-    
-    int mid = (start + end) / 2;
-    int left = make_st(start, mid);
-    int right = make_st(mid + 1, end);
-    st[node].l = left;
-    st[node].r = right;
-    return node;
+// pst, k번째로 작은 가중치
+
+int n;
+vector<Node> st; // 4*n or 1LL << ((int)ceil(log2(n)) + 1)으로 초기화
+vector<int> root, arr, par; // n으로 초기화
+
+T make_st(ll node, int start, int end) {
+	if (start == end)
+		return (st[node] = { -1, -1, 0 }).v; // 초기화 값 필요하다면 변경
+	int mid = (start + end) / 2;
+	st.push_back({ -1, -1, 0 });
+	st[node].l = st.size() - 1;
+	st.push_back({ -1, -1, 0 });
+	st[node].r = st.size() - 1;
+	T left = make_st(st[node].l, start, mid);
+	T right = make_st(st[node].r, mid + 1, end);
+	st[node].v = left + right;
+
+	return st[node].v;
 }
 
-// PST 업데이트 (안전한 push_back 방식)
-void update(int node, int start, int end, int index, int diff) {
-    if (start == end) {
-        st[node].v += diff;
-        return;
-    }
-    int mid = (start + end) / 2;
-    if (index <= mid) {
-        int old_l = st[node].l;
-        Node next_node = st[old_l]; // 복사본 생성 (참조 무효화 방지)
-        st.push_back(next_node);
-        st[node].l = st.size() - 1;
-        update(st[node].l, start, mid, index, diff);
-    } else {
-        int old_r = st[node].r;
-        Node next_node = st[old_r]; // 복사본 생성
-        st.push_back(next_node);
-        st[node].r = st.size() - 1;
-        update(st[node].r, mid + 1, end, index, diff);
-    }
-    st[node].v = st[st[node].l].v + st[st[node].r].v;
+void update(ll node, int start, int end, int index, T diff) {
+	if (start == end) {
+		st[node].v += diff;
+		return;
+	}
+	
+	int mid = (start + end) / 2;
+	if (index <= mid) {
+		st.push_back(st[st[node].l]);
+		st[node].l = st.size() - 1;
+		update(st[node].l, start, mid, index, diff);
+	}
+	else {
+		st.push_back(st[st[node].r]);
+		st[node].r = st.size() - 1;
+		update(st[node].r, mid + 1, end, index, diff);
+	}
+	
+	st[node].v = st[st[node].l].v + st[st[node].r].v;
 }
 
-// LCA를 위한 오일러 투어 세그먼트 트리
-void make_st2(int node, int start, int end) {
-    if (start == end) {
-        st2[node] = tour[start];
-        return;
-    }
-    int mid = (start + end) / 2;
-    make_st2(node * 2, start, mid);
-    make_st2(node * 2 + 1, mid + 1, end);
-    st2[node] = min(st2[node * 2], st2[node * 2 + 1]);
+T get_k(int start, int end, int a, int b, int lca, int lca2, int k) {
+	if (start == end) return start;
+
+	int diff = st[st[b].l].v + st[st[a].l].v - st[st[lca].l].v - st[st[lca2].l].v;
+	int mid = (start + end) / 2;
+	//cout << diff << " " << k << "\n";
+
+	if (diff >= k) return get_k(start, mid, st[a].l, st[b].l, st[lca].l, st[lca2].l, k);
+	else return get_k(mid + 1, end, st[a].r, st[b].r, st[lca].r, st[lca2].r, k - diff);
 }
 
-int get2(int node, int start, int end, int left, int right) {
-    if (left > end || right < start) return INF;
-    if (left <= start && end <= right) return st2[node];
-    int mid = (start + end) / 2;
-    return min(get2(node * 2, start, mid, left, right), get2(node * 2 + 1, mid + 1, end, left, right));
+// 오일러 투어 트릭, lca
+
+using T2 = ll;
+vector<T2> st2; // 4*n or 1LL << ((int)ceil(log2(n)) + 1)으로 초기화
+
+T2 make_st2(ll node, int start, int end) {
+	if (start == end)
+		return st2[node] = tour[start]; // 초기화 값 필요하다면 변경
+	int mid = (start + end) / 2;
+	T2 left = make_st2(node * 2, start, mid);
+	T2 right = make_st2(node * 2 + 1, mid + 1, end);
+	st2[node] = min(left, right);
+
+	return st2[node];
 }
 
-// K번째 수 찾기
-int get_k(int start, int end, int a, int b, int lca, int lca2, int k) {
-    if (start == end) return start;
-    int diff = st[st[a].l].v + st[st[b].l].v - st[st[lca].l].v - st[st[lca2].l].v;
-    int mid = (start + end) / 2;
-    if (diff >= k) return get_k(start, mid, st[a].l, st[b].l, st[lca].l, st[lca2].l, k);
-    else return get_k(mid + 1, end, st[a].r, st[b].r, st[lca].r, st[lca2].r, k - diff);
+T2 get2(ll node, int start, int end, int left, int right) {
+	if (left > end || right < start) return INF<int>;
+	if (left <= start && end <= right) return st2[node];
+
+	int mid = (start + end) / 2;
+	T2 l = get2(node * 2, start, mid, left, right);
+	T2 r = get2(node * 2 + 1, mid + 1, end, left, right);
+
+	return min(l, r);
 }
+vector<int> d, firstRoot;
+
 
 void dfs(int x, int p) {
-    range_val[x].first = ++cnt;
-    tour.push_back(range_val[x].first);
-    n2s.push_back(x);
-    par[x] = p;
-    firstTour[x] = tour.size() - 1;
+	range[x].first = ++cnt;
 
-    // PST 경로 생성
-    int prev_root = root[range_val[p].first];
-    Node root_copy = st[prev_root];
-    st.push_back(root_copy);
-    root.push_back(st.size() - 1);
-    
-    // 좌표 압축된 값으로 업데이트
-    int val_idx = lower_bound(sorted_d.begin(), sorted_d.end(), d[x]) - sorted_d.begin();
-    update(root.back(), 0, sorted_d.size() - 1, val_idx, 1);
+	tour.push_back(range[x].first);
+	n2s.push_back(x);
+	par[x] = p;
 
-    for (int nxt : adj[x]) {
-        if (nxt == p) continue;
-        dfs(nxt, x);
-        tour.push_back(range_val[x].first);
+	firstTour[x] = tour.size() - 1;
+
+	// p의 root를 찾아야 함
+	st.push_back(st[root[range[p].first]]);
+	root.push_back(st.size() - 1);
+	update(root.back(), 0, 1'000'000, d[x], 1);
+
+
+	for (int nxt : adj[x]) {
+		if (nxt == p) continue;
+		dfs(nxt, x);
+		tour.push_back(range[x].first);
+	}
+
+	range[x].second = cnt;
+}
+
+void solve(int CASE = -1) {
+	
+
+    cin >> n;
+	d.resize(n + 1); firstTour.resize(n + 1);
+	par.resize(n + 1);
+	range.resize(n + 1);
+	for (int i = 1; i <= n; i++) {
+		cin >> d[i];
+	}
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        
+        cin >> a >> b;
+		
+		adj[a].push_back(b);
+		adj[b].push_back(a);
     }
+
+	st.reserve(4'000'000);
+	st.push_back({ -1, -1, 0});
+	make_st(0LL, 0, 1'000'000);
+	root.push_back(0);
+	tour.push_back(0);
+	dfs(1, 0);
+
+	st2.resize(1LL << ((int)ceil(log2(tour.size())) + 1));
+	make_st2(1LL, 1, tour.size() - 1);
+
+	int m;
+
+	cin >> m;
+	while (m--) {
+		int i, j, k;
+
+		cin >> i >> j >> k;
+		int ti = firstTour[i];
+		int tj = firstTour[j];
+
+		if (ti > tj) swap(ti, tj);
+		int lca = get2(1LL, 1, tour.size() - 1, ti, tj);
+		//cout << i << " " << j << " " << n2s[lca] << "\n";
+		cout << get_k(0, 1'000'000, root[range[i].first], root[range[j].first], 
+			root[range[n2s[lca]].first], root[range[par[n2s[lca]]].first], k) << "\n";
+	}
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(NULL), cout.tie(NULL);
 
-    if (!(cin >> n)) return 0;
-    d.resize(n + 1);
-    for (int i = 1; i <= n; i++) cin >> d[i];
-    
-    sorted_d = d;
-    sort(sorted_d.begin() + 1, sorted_d.end());
-    sorted_d.erase(unique(sorted_d.begin() + 1, sorted_d.end()), sorted_d.end());
-
-    for (int i = 0; i < n - 1; i++) {
-        int a, b; cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-
-    st.reserve(4000000); // 메모리 재할당 방지
-    int base_root = make_st(0, sorted_d.size() - 1);
-    
-    // root[0]은 비어있는 트리의 루트 (parent of root 처리에 사용)
-    root.push_back(base_root);
-    tour.push_back(0); // 1-based indexing을 위해 dummy 추가
-    
-    dfs(1, 0);
-
-    st2.resize(tour.size() * 4);
-    make_st2(1, 1, tour.size() - 1);
-
-    cin >> m;
-    while (m--) {
-        int i, j, k; cin >> i >> j >> k;
-        int ti = firstTour[i], tj = firstTour[j];
-        if (ti > tj) swap(ti, tj);
-        
-        int lca_cnt = get2(1, 1, tour.size() - 1, ti, tj);
-        int lca_node = n2s[lca_cnt];
-        
-        int ans_idx = get_k(0, sorted_d.size() - 1, 
-                           root[range_val[i].first], root[range_val[j].first], 
-                           root[range_val[lca_node].first], root[range_val[par[lca_node]].first], k);
-        cout << sorted_d[ans_idx] << "\n";
+    //pre();
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
     }
 
     return 0;
